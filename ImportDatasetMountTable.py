0dbc90574bbac682109a1acc5deaaf8d2541a894
Amanda Vieira <108702135+amandav777@users.noreply.github.com>
2025-02-11 22:26:01 -0300

    feat: primerios exercicios

---
+++ ImportDatasetMountTable.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ???
+np.set_printoptions(suppress=True)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+sns.set_theme()
+
+
+
+# nome do arquivo
+file_name = "/content/enhanced_box_office_data(2000-2024)u.csv"
+
+# formar a tabela
+df = pd.read_csv(file_name)
+
+# imprimir cabecalho da tabela (?)
+print(df.head())
